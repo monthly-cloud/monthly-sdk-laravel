32b4d26bfc165ebc3f9ab239dbc590683d2d8cae
Michael Slowik <sl0wik@users.noreply.github.com>
2020-11-19 21:40:28 +0000

    Apply fixes from StyleCI

--- src/Services/TranslationService.php
+++ src/Services/TranslationService.php
@@ -8,60 +8,60 @@ use MonthlyStorage;
 
 class TranslationService
 {
-	/**
-	 * Cached translations.
-	 *
-	 * @var array
-	 */
-	protected $translations = [];
+    /**
+     * Cached translations.
+     *
+     * @var array
+     */
+    protected $translations = [];
 
-	/**
-	 * Translate key using storage dictionary.
-	 *
-	 * @param string $key
-	 * @param string|null $locale 
-	 * @return string
-	 */
-	public function translate($key, $locale = null)
-	{
-		if (empty($locale)) {
-			$locale = App::getLocale();
-		}
+    /**
+     * Translate key using storage dictionary.
+     *
+     * @param string $key
+     * @param string|null $locale
+     * @return string
+     */
+    public function translate($key, $locale = null)
+    {
+        if (empty($locale)) {
+            $locale = App::getLocale();
+        }
 
-		$translations = $this->getTranslations($locale);
+        $translations = $this->getTranslations($locale);
 
-		if (empty($translations[$key])) {
-			return $key;
-		}
+        if (empty($translations[$key])) {
+            return $key;
+        }
 
-		return $translations[$key];
-	}
+        return $translations[$key];
+    }
 
-	/**
-	 * Get array with translations for locale.
-	 *
-	 * @param string|null $locale
-	 * @return array
-	 */
-	public function getTranslations($locale = null)
-	{
-		if (empty($locale)) {
-			$locale = App::getLocale();
-		}
+    /**
+     * Get array with translations for locale.
+     *
+     * @param string|null $locale
+     * @return array
+     */
+    public function getTranslations($locale = null)
+    {
+        if (empty($locale)) {
+            $locale = App::getLocale();
+        }
 
-		if (empty($this->translations[$locale])) {
-	    	// Get translations from storage.
-	        $translations = MonthlyStorage::endpoint('locales')
-	        	->locale($locale)
-	        	->get();
+        if (empty($this->translations[$locale])) {
+            // Get translations from storage.
+            $translations = MonthlyStorage::endpoint('locales')
+                ->locale($locale)
+                ->get();
 
-	        // Turn object to array.
-	        $translations = json_decode(json_encode($translations), true);
+            // Turn object to array.
+            $translations = json_decode(json_encode($translations), true);
 
-	        // Multi dimensions array to dot array.
-	        $this->translations[$locale] = Arr::dot($translations);
-		}
+            // Multi dimensions array to dot array.
+            $this->translations[$locale] = Arr::dot($translations);
+        }
 
-		return $this->translations[$locale];
-	}
+        return $this->translations[$locale];
+    }
 }
