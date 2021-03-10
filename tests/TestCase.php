5c2ee7efd9147f77da0c45ec690d6e6cd0ee6c43
Michael Slowik <sl0wik@users.noreply.github.com>
2021-03-10 14:21:19 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,18 +7,18 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-	protected function getPackageProviders($app)
-	{
-	    return [
-	    	\MonthlyCloud\Laravel\MonthlyCloudServiceProvider::class
-	    ];
-	}
+    protected function getPackageProviders($app)
+    {
+        return [
+            \MonthlyCloud\Laravel\MonthlyCloudServiceProvider::class,
+        ];
+    }
 
-	protected function getPackageAliases($app)
-	{
-	    return [
-	    	'MonthlyCloud' => \MonthlyCloud\Laravel\MonthlyCloud::class,
-	        'MonthlyStorage' => \MonthlyCloud\Laravel\MonthlyStorage::class,
-	    ];
-	}
+    protected function getPackageAliases($app)
+    {
+        return [
+            'MonthlyCloud' => \MonthlyCloud\Laravel\MonthlyCloud::class,
+            'MonthlyStorage' => \MonthlyCloud\Laravel\MonthlyStorage::class,
+        ];
+    }
 }
