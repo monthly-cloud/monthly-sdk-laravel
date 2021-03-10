ce2fd39db33817bf0dd763679afa162f65617a79
Michael Slowik <slowik.michael@gmail.com>
2021-03-10 09:21:04 -0500

    Initial tests

---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MonthlyCloud\Laravel\Test;
+
+use MonthlyCloud\Sdk\StorageBuilder;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+	protected function getPackageProviders($app)
+	{
+	    return [
+	    	\MonthlyCloud\Laravel\MonthlyCloudServiceProvider::class
+	    ];
+	}
+
+	protected function getPackageAliases($app)
+	{
+	    return [
+	    	'MonthlyCloud' => \MonthlyCloud\Laravel\MonthlyCloud::class,
+	        'MonthlyStorage' => \MonthlyCloud\Laravel\MonthlyStorage::class,
+	    ];
+	}
+}
